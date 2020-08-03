47cf2210403e24cf63d92ce0ba3df539588e82e9
Christine Yang <christine.yang@yale.edu>
2020-08-03 15:31:40 -0400

    visual fixes

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 
 
 window.onscroll = function() {scrollFunction()};
-var elementTarget = document.getElementById("contact");
+var pink = document.getElementById("contact");
+var blue = document.getElementById("covid");
+var orange = document.getElementById("signup");
 
   function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("navbar").style.height = "9%";
       document.getElementById("navbar").style.borderRadius = "0px";
       document.getElementById("navlinks").style.fontSize = "15px";
-  } else {
+  }
+  else 
+  { 
       document.getElementById("navbar").style.height = "12%";
       document.getElementById("navbar").style.borderRadius = "0px 0px 20px 20px";
       document.getElementById("navlinks").style.fontSize = "18px";
       
   }
   
-  if (window.scrollY > elementTarget.offsetTop - 1) {
-      document.getElementById("logo").style.filter = "hue-rotate(-28deg)";
-      document.getElementById("navlinks").style.filter = "hue-rotate(-28deg)";
-  }
-  else
-  {
-      document.getElementById("logo").style.filter = "hue-rotate(0)";
-      document.getElementById("navlinks").style.filter = "hue-rotate(0)";
-  }
+    if (window.scrollY > pink.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(-28deg)";
+    }
+    else if (window.scrollY > orange.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(0)";
+    }
+    else if (window.scrollY > blue.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(200deg)";
+    }
+    else
+    {
+        document.getElementById("navbar").style.filter = "hue-rotate(0)";
+    }
    }
 
 
